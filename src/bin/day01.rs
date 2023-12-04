962c5fc78acb1cc014143f3d735fe8ff9a212f6f
Sami J. Mäkinen <sjm@iki.fi>
2023-12-05 01:26:20 +0200

    Day 2 done

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -6,29 +6,62 @@ use clap::Parser;
 use log::*;
 use std::io::{self, BufRead};
 
+const NUMS: [(&str, u32); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
 fn main() -> anyhow::Result<()> {
     let mut opts = OptsCommon::parse();
     opts.finish()?;
     opts.start_pgm(env!("CARGO_BIN_NAME"));
 
-    let mut n = 0;
-    let mut sum = 0;
+    let (mut n, mut sum) = (0, 0);
     for line in io::stdin().lock().lines() {
         n += 1;
         let line = line?;
-        let lt = line.trim();
-        let d1 = lt
-            .chars()
-            .find_map(|c| if c.is_numeric() { c.to_digit(10) } else { None })
-            .ok_or_else(|| anyhow!("No first digit"))?;
-        let d2 = lt
-            .chars()
-            .rev()
-            .find_map(|c| if c.is_numeric() { c.to_digit(10) } else { None })
-            .ok_or_else(|| anyhow!("No last digit"))?;
+        let line = line.trim();
 
-        info!("#{n:0>4} {d1}{d2} <-- {lt}");
-        sum += (d1 as u64) * 10 + d2 as u64;
+        let c_vec = NUMS
+            .iter()
+            .filter_map(|n| Some((line.find(n.0)?, n.1)))
+            .chain(NUMS.iter().filter_map(|n| Some((line.rfind(n.0)?, n.1))))
+            .collect::<Vec<(usize, u32)>>();
+
+        let d_vec = line
+            .chars()
+            .enumerate()
+            .filter_map(|c| match c.1.is_numeric() {
+                true => Some((c.0, c.1.to_digit(10)?)),
+                _ => None,
+            })
+            .collect::<Vec<(usize, u32)>>();
+        debug!("c_vec {c_vec:?} d_vec {d_vec:?}");
+
+        let n1 = c_vec
+            .iter()
+            .chain(d_vec.iter())
+            .min_by_key(|i| i.0)
+            .ok_or_else(|| anyhow!("no num"))?
+            .1;
+
+        let n2 = c_vec
+            .iter()
+            .chain(d_vec.iter())
+            .max_by_key(|i| i.0)
+            .ok_or_else(|| anyhow!("no num"))?
+            .1;
+
+        let num = 10 * (n1 as u64) + n2 as u64;
+        info!("#{n:0>4} {num} <-- {line}");
+        sum += num;
 
         if let Some(max) = opts.max_iter {
             if n >= max {
---
+++ src/bin/day02.rs
@@ -0,0 +1,64 @@
+// bin/sjmb.rs
+
+use anyhow::anyhow;
+use aoc2023::*;
+use clap::Parser;
+use log::*;
+use std::collections::HashMap;
+use std::io::{self, BufRead};
+
+fn main() -> anyhow::Result<()> {
+    let mut opts = OptsCommon::parse();
+    opts.finish()?;
+    opts.start_pgm(env!("CARGO_BIN_NAME"));
+
+    let limits = HashMap::from([("blue", 14), ("green", 13), ("red", 12)]);
+    debug!("Limits: {limits:?}");
+
+    let (mut sum, mut sum_p) = (0, 0);
+    for line in io::stdin().lock().lines() {
+        let line = line?;
+        let line = line.trim();
+
+        let mut pass = true;
+        let (g_id, rounds) = line
+            .split_once(':')
+            .ok_or_else(|| anyhow!("parse error: line"))?;
+        let (g_id, rounds) = (g_id.trim(), rounds.trim());
+        let id = g_id
+            .split_once(' ')
+            .ok_or_else(|| anyhow!("parse error: game num"))?
+            .1
+            .parse::<i32>()?;
+
+        let (mut p, mut max) = (1, HashMap::new());
+        for r in rounds.split(';') {
+            let r = r.trim();
+            for n_c in r.split(',') {
+                let n_c = n_c.trim();
+                let (n, c) = n_c
+                    .split_once(' ')
+                    .ok_or_else(|| anyhow!("parse error: {n_c}"))?;
+                let n = n.parse::<i32>()?;
+                if n > *limits.get(c).ok_or_else(|| anyhow!("Unknown color {c}"))? {
+                    pass = false;
+                }
+
+                if n > *max.get(c).unwrap_or(&0) {
+                    max.insert(c, n);
+                }
+            }
+        }
+        max.values().for_each(|v| p *= *v);
+
+        info!("id {id} {pass} {p}");
+        sum_p += p;
+        if pass {
+            sum += id;
+        }
+    }
+
+    println!("Sum: {sum} Sum-p: {sum_p}");
+    Ok(())
+}
+// EOF
