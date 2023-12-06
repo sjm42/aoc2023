2f3a3e6cbd1b07ffee4f930db46b5be0ad81c504
Sami J. Mäkinen <sjm@iki.fi>
2023-12-06 20:14:16 +0200

    Day 04 works

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -13,7 +13,10 @@ fn main() -> anyhow::Result<()> {
     opts.finish()?;
     opts.start_pgm(env!("CARGO_BIN_NAME"));
 
+    let mut cards = Vec::new();
+    let mut card_wins = Vec::new();
     let mut score_sum = 0;
+
     let mut n = 0;
     for line in io::stdin().lock().lines() {
         let line = line?;
@@ -33,7 +36,7 @@ fn main() -> anyhow::Result<()> {
             .split_whitespace()
             .map(|n| n.parse::<i32>().unwrap_or(-1))
             .collect::<Vec<_>>();
-        debug!("Winning[#{n}]: {win_v:?}");
+        trace!("Winning[#{n}]: {win_v:?}");
         let mut w_hash = HashSet::new();
         for v in win_v.iter() {
             w_hash.insert(*v);
@@ -43,11 +46,12 @@ fn main() -> anyhow::Result<()> {
             .split_whitespace()
             .map(|n| n.parse::<i32>().unwrap_or(-1))
             .collect::<Vec<_>>();
-        debug!("Have[#{n}]: {have_v:?}");
+        trace!("Have[#{n}]: {have_v:?}");
 
-        let mut score = 0;
+        let (mut win_cnt, mut score) = (0, 0);
         for h in have_v.iter() {
             if w_hash.contains(h) {
+                win_cnt += 1;
                 score = match score {
                     0 => 1,
                     n => n * 2,
@@ -55,7 +59,10 @@ fn main() -> anyhow::Result<()> {
             }
         }
 
-        info!("Score #{n} {score}");
+        cards.push(n as i32);
+        card_wins.push(win_cnt);
+
+        info!("Card#{n} wins: {win_cnt} score: {score}");
         score_sum += score;
 
         n += 1;
@@ -65,9 +72,29 @@ fn main() -> anyhow::Result<()> {
             }
         }
     }
-
     println!("Score: {score_sum}");
+
+    let cards = calc_cards(&card_wins, cards, 0);
+    println!("Cards# {num}", num = cards.len());
+
     Ok(())
 }
 
+fn calc_cards(wins: &[i32], mut cards: Vec<i32>, r: usize) -> Vec<i32> {
+    let mut copies = Vec::new();
+    for c in cards.iter() {
+        let win_count = wins[*c as usize];
+        trace!("Calc{r} Card#{c} wins {win_count}");
+        (*c..*c + win_count).for_each(|n| copies.push(n + 1));
+        trace!(" calc{r} copies: {copies:?}");
+    }
+    trace!(" calc[{r}] all copies: {copies:?}");
+    if !copies.is_empty() {
+        let mut new_copies = calc_cards(wins, copies, r + 1);
+        trace!("  calc[{r}] new copies: {new_copies:?}");
+        cards.append(&mut new_copies);
+    }
+
+    cards
+}
 // EOF
